05dcc3e80e1b8a90c8c2b04ca2eecab5faa5a04a
vanloc19bk96 <vanlocbk1996@gmail.com>
2019-08-29 23:22:26 +0700

    image blending

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -4,6 +4,9 @@ image1 = cv2.imread('messi.jpg')
 image2 = cv2.imread('ronaldo.jpg')
 image2 = cv2.resize(image2, (image1.shape[1], image1.shape[0]))
 
-image = cv2.add(image1, image2)
-cv2.imshow('image', image)
+# image = cv2.add(image1, image2)
+
+# image blending
+blending = cv2.addWeighted(image1, 0.3, image2, 0.7, 0)
+cv2.imshow('image', blending)
 cv2.waitKey(0)
